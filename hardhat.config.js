c6a3fd5ba4ea803e94111a09a3d4e21de3646294
Boban Milošević <milosevic.boban@gmail.com>
2022-06-06 11:20:19 +0200

    Added Hardhat tasks for checking account balances

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,34 @@ require('@nomiclabs/hardhat-waffle')
 const { generateInfuraUrl, generateAlchemyUrl } = require('./utils/generate-url')
 const { ALCHEMY_API_KEY, INFURA_API_KEY, PRIVATE_KEYS } = require('./.credentials')
 
+task("balance", "Prints an account's balance")
+    .addParam("address", "The account's address")
+    .setAction(async (args) => {
+        const balance = await ethers.provider.getBalance(args.address)
+        console.log(`${args.address} = ${ethers.utils.formatEther(balance)} ETH`)
+    })
+
+task("balances", "Prints balances of all configured accounts")
+    .setAction(async () => {
+        const accounts = await ethers.getSigners()
+
+        if (!accounts.length) {
+            console.log('No accounts detected. Configure them in your Hardhat config file.')
+
+            return
+        }
+
+        console.log(
+            `${accounts.length} accounts configured\n\n` +
+            'ACCOUNT                                      BALANCE ETH\n' +
+            '-----------------------------------------------------------------'
+        )
+        for (const account of accounts) {
+            const balance = await ethers.provider.getBalance(account.address)
+            console.log(`${account.address}   ${ethers.utils.formatEther(balance)}`)
+        }
+    })
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
