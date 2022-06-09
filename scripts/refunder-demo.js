d6e1b00c07d3a0e06f09d7a39d4e1b084bf8a393
Boban Milošević <milosevic.boban@gmail.com>
2022-06-09 13:16:52 +0200

    CLI demo now creates contract instance using getContractAt(), instead of using hardcoded ABI

--- scripts/refunder-demo.js
+++ scripts/refunder-demo.js
@@ -28,23 +28,17 @@
 
 */
 
+const { ethers } = require('hardhat')
+
 // const CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3' // this is where your first contract on local Hardhat blockchain will be deployed to
 const CONTRACT_ADDRESS = '0xacCDe34BC1391aa8D3FEF3Bfaa568dFd9D1DA791' // this is the already deployed contract on Ropsten
-const CONTRACT_ABI = [
-    'event PaymentReceived (address indexed from, uint amount)',
-    'event AccountRefunded (address indexed to, uint amount)',
-    'function balances (address) view public returns (uint)',
-    'function paymentsCount () view public returns (uint)',
-    'function refundsCount () view public returns (uint)',
-    'function refund () public',
-]
 
 const amount = 0.01
 
 async function main () {
 
     const signer = await ethers.getSigner()
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer)
+    const contract = await ethers.getContractAt('Refunder', CONTRACT_ADDRESS, signer)
 
     console.log('\nINITIAL STATE')
     await printBalance(signer.address)
