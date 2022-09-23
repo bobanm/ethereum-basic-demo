863856ae2c42034c9e60c6a5d014f5ff83895fb4
Boban Milošević <milosevic.boban@gmail.com>
2022-09-23 13:34:35 +0200

    Removed support non-Goerli testnets, added supported for L2 Goerli testnets

--- frontend/config.js
+++ frontend/config.js
@@ -8,12 +8,9 @@ const CONTRACT_ABI = [
 ]
 
 const deploymentConfig = new Map([
-    [3,      { address: '0xacCDe34BC1391aa8D3FEF3Bfaa568dFd9D1DA791', blockNumber: 12277906, networkName: 'ropsten' }],
-    [4,      { address: '0x177dd8098dD548f8206067AFFE101D6C8288B19D', blockNumber: 10778311, networkName: 'rinkeby' }],
     [5,      { address: '0x177dd8098dD548f8206067AFFE101D6C8288B19D', blockNumber: 6987150,  networkName: 'goerli' }],
-    [42,     { address: '0x177dd8098dD548f8206067AFFE101D6C8288B19D', blockNumber: 31946295, networkName: 'kovan' }],
-    [69,     { address: '0x177dd8098dD548f8206067AFFE101D6C8288B19D', blockNumber: 3629298,  networkName: 'optimism-kovan' }],
-    [421611, { address: '0x177dd8098dD548f8206067AFFE101D6C8288B19D', blockNumber: 12240862, networkName: 'arbitrum-rinkeby' }],
+    [420,    { address: '0x177dd8098dD548f8206067AFFE101D6C8288B19D', blockNumber: 1374064,  networkName: 'optimism-goerli' }],
+    [421613, { address: '0x177dd8098dD548f8206067AFFE101D6C8288B19D', blockNumber: 392644,   networkName: 'arbitrum-goerli' }],
     [31337,  { address: '0x5FbDB2315678afecb367f032d93F642f64180aa3', blockNumber: 0,        networkName: 'hardhat' }],
 ])
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,6 @@
 require('@nomiclabs/hardhat-waffle')
 
-// for demo purposes, we'll access some networks using Infura, and some others using alchemy
-const { generateInfuraUrl, generateAlchemyUrl } = require('./utils/generate-url')
-const { ALCHEMY_API_KEY, INFURA_API_KEY, PRIVATE_KEYS } = require('./.credentials')
+const { ALCHEMY_API_KEY, PRIVATE_KEYS } = require('./.credentials')
 
 task("balance", "Prints an account's balance")
     .addParam("address", "The account's address")
@@ -39,28 +37,16 @@ module.exports = {
     solidity: "0.8.13",
 
     networks: {
-        ropsten: {
-            url: generateAlchemyUrl('ropsten', ALCHEMY_API_KEY),
-            accounts: PRIVATE_KEYS,
-        },
-        rinkeby: {
-            url: generateInfuraUrl('rinkeby', INFURA_API_KEY),
-            accounts: PRIVATE_KEYS,
-        },
         goerli: {
-            url: generateAlchemyUrl('goerli', ALCHEMY_API_KEY),
+            url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
             accounts: PRIVATE_KEYS,
         },
-        kovan: {
-            url: generateInfuraUrl('kovan', INFURA_API_KEY),
+        'optimism-goerli': {
+            url: 'https://goerli.optimism.io',
             accounts: PRIVATE_KEYS,
         },
-        'optimism-kovan': {
-            url: 'https://kovan.optimism.io',
-            accounts: PRIVATE_KEYS,
-        },
-        'arbitrum-rinkeby': {
-            url: 'https://rinkeby.arbitrum.io/rpc',
+        'arbitrum-goerli': {
+            url: 'https://goerli-rollup.arbitrum.io/rpc/',
             accounts: PRIVATE_KEYS,
         },
     }
